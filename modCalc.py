01a4b94e60e52f5a2ae5d76782e7476cf5003d2f
toledod <toledod@carleton.edu>
2024-09-27 16:14:53 -0500

    pythoncode for hw

---
+++ modCalc.py
@@ -0,0 +1,15 @@
+
+
+# for x in range(1,98):
+#     if (7**x)%97 == 53:
+#         print("x is ", x)
+
+# for y in range(1,98):
+#     if (7**y)%97 == 82:
+#         print("y is ", y)
+
+a = (82**22)%97 
+print("a is ", a)
+
+b = (53**41)%97 
+print("b is ", b)
